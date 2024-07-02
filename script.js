399c12f5634908b2a4b733430ab31e234f9c5206
JassJessi <saraivajessi@gmail.com>
2024-07-02 09:08:16 -0300

    Adicionando perguntas no JavaScript.

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
-let pergunta = {
+let perguntas = [
+    
+{
     titulo: 'Gato',
     alternativas: ['Gat', 'Cat', 'Gate', 'Dog'],
     correta: 1
-};
+},
 
-function start(){
-    let alts = document.querySelectorAll('.alternativa');
-    alts.forEach(function(element,index){
-        element.addEventListener('click', function(){
-            console.log("Checa resposta")
-        })
-    })
+{
+    titulo: 'Fire',
+    alternativas: ['Fogo', 'Água', 'Terra', 'Ar'],
+    correta: 0
+},
 
-    mostraquestao(pergunta);
+{
+    titulo: 'Bird',
+    alternativas: ['Gato', 'Ururbu', 'Pombo', 'Passaro'],
+    correta: 3
 }
 
-function mostraquestao(q){
+]
+
+let app = {
+//Transformando a função em metódo
+start: function(){
+
+    this.Atualpos = 0;
+    this.Totalpontos = 0;
+
+    let alts = document.querySelectorAll('.alternativa');
+    alts.forEach((element,index)=>{
+        element.addEventListener('click', ()=>{
+            this.checaResposta(index);        
+        })
+    })
+    this.atualizaPontos();
+    app.mostraquestao(perguntas[this.Atualpos]);
+},
+
+mostraquestao: function(q){
+    this.qatual = q;
     //mostrando o titulo
     let titleDiv = document.getElementById('titulo');
     titleDiv.textContent = q.titulo;
@@ -24,11 +47,41 @@ function mostraquestao(q){
     alts.forEach(function(element,index){
         element.textContent = q.alternativas[index];
         })
-    
+
+},
+
+Proximaperg: function(){
+    this.Atualpos++;
+    if(this.Atualpos == perguntas.length){
+        this.Atualpos = 0;
+    }
+},
+
+checaResposta: function(user){
+    if(this.qatual.correta == user){
+        console.log("Correta")
+        this.Totalpontos++;
+    }
+    else{
+        console.log("Errada")
+    }
+    this.atualizaPontos();
+    this.Proximaperg();
+    this.mostraquestao(perguntas[this.Atualpos]);
+},
+
+atualizaPontos: function(){
+    let scoreDiv = document.getElementById('pontos');
+    scoreDiv.textContent = `Sua pontuação é: ${this.Totalpontos}`;
+} 
 
 }
 
-start(mostraquestao)
+
+app.start();
+
+
+
 /* Ouvinte de eventos: Avisa que o usuário clicou em algum lugar da tela
 
 let btn = document.getElementById('btn');
@@ -36,3 +89,4 @@ btn.addEventListener('click',function() {
     console.log("O cara clicou ali oh!");
 })
 */
+
